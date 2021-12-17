ab48605dee221fe27e33db76d2adcdb956bb79c9
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2021-12-17 02:15:48 +0100

    Fine es del 16/12/2021

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,31 @@
 <template>
     <section class="list-container">
-        <Card/>
+        <div class="card" v-for="(album, index) in albums" :key="index">
+            <Card :info="album"/>
+        </div>
     </section>
 </template>
 
 <script>
+import axios from 'axios';
 import Card from './subsections/Card.vue';
+
 export default {
     name: 'Main',
     components: {
         Card,
-    }
+    }, 
+    data() {
+        return {
+            albums: null
+        }
+    },
+    created() {
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then((albums) => {
+            this.albums = albums.data.response;
+        })
+    },
 }
 </script>
 
@@ -20,5 +35,15 @@ export default {
         margin: 30px auto;
         display: flex;
         flex-wrap: wrap;
+        // Proprietà card
+        .card {
+        padding: 20px;
+        min-height: 370px;
+        max-height: 370px;
+        overflow-y: scroll;
+        width: calc(20% - 40px);
+        margin: 10px 20px;
+        background-color: #2e3a46;
+        }
     }
 </style>
--- src/components/subsections/Card.vue
+++ src/components/subsections/Card.vue
@@ -1,38 +1,35 @@
 <template>
-    <div class="card">
-        <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg">
-        <h3>New Jersey</h3>
-        <span class="name">Bon Jovi</span>
-        <span class="date">1988</span>
+    <div class="album">
+        <img :src="info.poster">
+        <h3>{{info.title}}</h3>
+        <span class="name">{{info.author}}</span>
+        <span class="date">{{info.year}}</span>
     </div>
 </template>
 
 <script>
 export default {
     name: 'Card',
+    props: {
+        info: Object
+    }
 }
 </script>
 
 <style lang="scss" scoped>
-    .card {
-        padding: 20px;
+    .album {
         display: flex;
         flex-direction: column;
         align-items: center;
-        min-height: 350px;
-        max-height: 350px;
-        overflow-y: scroll;
-        width: calc(20% - 40px);
-        margin: 10px 20px;
-        background-color: #2e3a46;
         img {
             width: 100%;
         }
         h3 {
             margin: 15px 0 10px;
-            font-size: 26px;
+            font-size: 24px;
             text-transform: uppercase;
             color: #fdfefd;
+            text-align: center;
         }
         .name {
             color: #726f66;
